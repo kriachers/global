6608ee76adc6e2066c9f0451973b677888bf9a70
Liza <liza@MacBook-Air-Elizaveta.local>
2022-08-13 16:34:22 +0300

    update

--- js/animation.js
+++ js/animation.js
@@ -1,20 +1,16 @@
 let lastAnimation = document.querySelector('.main-graphics-counties__five');
-
-
 let firstAnimation = document.querySelector('.main-graphics-counties__first');
 let secondAnimation = document.querySelector('.main-graphics-counties__second');
 let thirdAnimation = document.querySelector('.main-graphics-counties__third');
 let fourAnimation = document.querySelector('.main-graphics-counties__four');
-
 let mainAnimation = document.querySelector('.main-graphic__animation')
 
-
-
+// animation 
 lastAnimation.addEventListener('animationend', () => {
     mainAnimation.classList.add('rotate');
 })
 
-
+// burger menu
 let hamburgerButton = document.querySelector('.hamburger');
 let hamburgerMenu = document.querySelector('.hamburger__menu');
 let HamburgerIcon = document.querySelector('.hamburger')
@@ -24,8 +20,7 @@ hamburgerButton.addEventListener('click', function() {
     HamburgerIcon.classList.toggle('active');
 })
 
-
-
+// search
 let inputData = document.querySelector('.main-wrapper-search__input');
 let inputBtn = document.querySelector('.main-wrapper-search__button');
 let warning = document.querySelector('.main__warning');
@@ -36,14 +31,11 @@ inputData.oninput = function() {
 
     const textLength = inputData.value.length;
     if (textLength > 4 & textLength < 10) {
-        inputData.getElementsByClassName.borderColor = 'red';
         inputBtn.classList.remove('disabled')
         warning.classList.remove('active')
     } else { 
         inputBtn.classList.add('disabled')
         warning.classList.add('active')
-    }
-
-    
+    }    
 }
 
